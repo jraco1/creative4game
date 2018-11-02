17d57ade0aa5bd2c8bae50ebe11d0bf79004fc34
Jacob Hensley <jraco1@hotmail.com>
2018-11-02 18:05:14 +0000

    Chat working

--- clonegar/public/javascripts/clonegar.js
+++ clonegar/public/javascripts/clonegar.js
@@ -1,7 +1,8 @@
 /* global angular */
+/* global io */
 angular.module('clonegarApp', [])
     .controller('clonegarCtrl', clonegarCtrl)
-	.directive('player', playerDirective);
+    .directive('player', playerDirective);
 
 // var express = require('express');
 // var http = require('http').Server(express);
@@ -9,50 +10,50 @@ angular.module('clonegarApp', [])
 
 function clonegarCtrl($scope) {
 
+    // $scope.msgs = new Array();
+    $scope.msgs = [];
+    $scope.players = [];
+    
+    // io appears to be declared in 
+    // <script src="/socket.io/socket.io.js"></script> in the html
+    // var socket = io.connect('http://52.15.33.242:4200');
+    var socket = io.connect();
+
+    socket.on('connect', function(data) {
+        socket.emit('join', 'Hello World from client');
+    });
+
+    socket.on('broadcast msg', function(data) {
+        // console.log ("broadcast message received");
+        // console.log (data);
+        $scope.msgs.push (data.user + ": " + data.msg);
+        // Needed to make angular update the page immediately.
+        $scope.$apply();
+        // Scrolls chat to bottom on new message.
+        var msgs = document.getElementById("chat");
+        msgs.scrollTop = msgs.scrollHeight;
+    });
+
+    $scope.sendMsg = function() {
+        // console.log("Sending message");
+        socket.emit('chat message', { user: $scope.username, msg: $scope.message });
+        $scope.message = '';
+    };
 }
 
-// io appears to be declared in 
-// <script src="/socket.io/socket.io.js"></script> in the html
-var socket = io.connect('http://52.15.33.242:4200');
-socket.on('connect', function(data) {
-    socket.emit('join', 'Hello World from client');
-});
-
-socket.on('messages', function(data) {
-    alert(data);
-});
-// io.on ('connection', function (socket){
-//     console.log ('a user connected');
-// });
-
 function playerDirective() {
-	return {
-		scope: {
-			player: '='
-		},
-		restrict: 'E',
-		replace: 'true',
-		template: (
-			
-			'<div class="player">' +
-			'{{player.name}} <span class="dot"></span>' +
-			'</div>'
-			
-// 			'<div class="definition">' +
-// 			'<div class="flip-box">' +
-// 			'<div class="flip-box-inner">' +
+    return {
+        scope: {
+            player: '='
+        },
+        restrict: 'E',
+        replace: 'true',
+        template: (
 
-// 			'<div class="flip-box-front">' +
-// 			'<h1>{{definition.type}}</h1></div>' +
-
-// 			'<div class="flip-box-back">' +
-// 			'<h4>Definition</h4>' +
-// 			'<p>{{definition.definition}}</p>' +
-// 			'<h4>Example</h4>' +
-// 			'<p>{{definition.example}}</p></div>' +
-
-// 			'</div></div></div>'
-		)
-	};
+            '<div class="player">' +
+            '{{player.name}} <span class="dot"></span>' +
+            '</div>'
+        )
+    };
 
 }
